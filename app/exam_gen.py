0fdb575c227cac710a878bb9be629bb922e84504
melchanatan <chanatan@outlook.com>
2024-09-23 16:05:54 +0700

    feat: change system prompt

--- app/exam_gen.py
+++ app/exam_gen.py
@@ -1,12 +1,25 @@
+from typing import List
 from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.vectorstores import FAISS
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
-from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.embeddings import OpenAIEmbeddings
 
-example_query = "สมมุติให้คุณเป็นอาจารย์ที่นำข้อสอบนี้ไปออกเป็นข้อสอบฉบับใหม่ของตัวเองที่มีเนื้อหา แต่ละข้อ คล้ายเคียงจากนี้ เช็คด้วยถ้าเหมือน เปลี่ยนโจทย์ เท่าจำนวนข้อที่มี พร้อมเฉลยแบบละเอียดที่เข้าใจง่าย เป็นภาษาไทย พร้อมตรวจทานวิธีคิดว่าคำตอบที่เฉลยละเอียดตรงกับคำตอบที่ถูกต้องหรือไม่ ถ้าไม่มีคำตอบจงสร้างโจทย์ข้อใหม่ที่มีคำตอบ และตอบให้อยู่ในรูป format json เช่น topic ( มีครั้งเดียวในไฟล์ละอยู่บนสุด เป็นชื่อหัวข้อของไฟล์นั้น ) , describe ( ถ้ามีคำคำสั่ง ขอคำอธิบายที่ชัดเจนและยาวพอที่จะอธิบายได้ครอบครุม ) และที่มีทุกข้อคือ question: , options , answer , explanation และ {type (  math, thai , eng , social)} โดยคำตอบเป็น ช้อย 1 2 3 4 และ เฉลยเป็น เลข 1 2 3 4  และ ตั้งหัวข้อจาก หัวข้อที่มีมาให้ ถ้าไม่มีให้ตั้งจากเนื้อหาโดยรวมแบบสั้นๆ และกระซับได้ใจความ และ สลับลำดับของข้อโจทย์และเรียงเลขข้อใหม่โดยเริ่มที่ 1 "
+# example_query = "สมมุติให้คุณเป็นอาจารย์ที่นำข้อสอบนี้ไปออกเป็นข้อสอบฉบับใหม่ของตัวเองที่มีเนื้อหา แต่ละข้อ คล้ายเคียงจากนี้ เช็คด้วยถ้าเหมือน เปลี่ยนโจทย์ เท่าจำนวนข้อที่มี พร้อมเฉลยแบบละเอียดที่เข้าใจง่าย เป็นภาษาไทย พร้อมตรวจทานวิธีคิดว่าคำตอบที่เฉลยละเอียดตรงกับคำตอบที่ถูกต้องหรือไม่ ถ้าไม่มีคำตอบจงสร้างโจทย์ข้อใหม่ที่มีคำตอบ และตอบให้อยู่ในรูป format json เช่น topic ( มีครั้งเดียวในไฟล์ละอยู่บนสุด เป็นชื่อหัวข้อของไฟล์นั้น ) , describe ( ถ้ามีคำคำสั่ง ขอคำอธิบายที่ชัดเจนและยาวพอที่จะอธิบายได้ครอบครุม ) และที่มีทุกข้อคือ question: , options , answer , explanation และ {type (  math, thai , eng , social)} โดยคำตอบเป็น ช้อย 1 2 3 4 และ เฉลยเป็น เลข 1 2 3 4  และ ตั้งหัวข้อจาก หัวข้อที่มีมาให้ ถ้าไม่มีให้ตั้งจากเนื้อหาโดยรวมแบบสั้นๆ และกระซับได้ใจความ และ สลับลำดับของข้อโจทย์และเรียงเลขข้อใหม่โดยเริ่มที่ 1 "
+
+
+system_prompt = "you are a exam question generating tutor, **you always answer in thai language**. you are task with generating question from 4 categories (math, thai, english, social) each question consisted of a question, 4 options, 1 correct answer and an explanation."
+format_prompt = """
+answer in JSON format:
+[{
+    question: string,
+    options: [string],
+    answer: string,
+    explanation: string
+}, {...}]
+"""
 
 embeddings = OpenAIEmbeddings()
 
@@ -24,10 +37,12 @@ chain = ConversationalRetrievalChain.from_llm(
 )
 
 
-# def run(url: str) -> str:
-#     output = chain.run(query)
-#     return output
+def run(query: str) -> str:
+    output = chain.invoke(
+        {"question": query, "chat_history": [system_prompt, format_prompt]})
+    return output
 
 
-# if __name__ == "__main__":
-#     print(run("hello"))
+if __name__ == "__main__":
+    print("--------------------------")
+    print(run("generate 3 question in math catergory"))
--- app/main.py
+++ app/main.py
@@ -4,5 +4,5 @@ import os
 
 os.environ["OPENAI_API_KEY"] = os.environ.get("OPENAI_API_KEY", "sk-********")
 
-app = create_service("exam_gen:chain")
+app = create_service("exam_gen:run")
 # app = create_service("example:chain")
