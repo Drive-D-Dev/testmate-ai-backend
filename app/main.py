8eae5bae72a41b7a71a463d0adfe49dc9b85d344
Sunthewhat <chotiwet.wisi@icloud.com>
2024-09-26 12:59:28 +0700

    add path to test connection

--- app/main.py
+++ app/main.py
@@ -1,14 +1,19 @@
-
 from langcorn import create_service
 import os
 from fastapi import Body, FastAPI, Response
 import mistake_gen
+
 os.environ["OPENAI_API_KEY"] = os.environ.get("OPENAI_API_KEY", "sk-********")
 
 # app = create_service("mistake_gen:run")
 # app = create_service("example:chain")
 
 app = FastAPI()
+
+
+@app.get("/")
+async def hello():
+    return Response(content="Hello From FastAPI")
 
 
 @app.post("/generate_mistake")
